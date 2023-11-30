6eabe227bca50f649ff889018b268813eddcf83d
HiddeWierper <hidde@wierper.net>
2023-11-30 19:02:27 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <meta charset="utf-8">
+    <title>My first PHP page</title>
+  </head>
+  <body>
+    <h1>Opdracht 1</h1> 
+    <?php
+      $a = "Ik heb niet gefaald";
+      $b = "die niet werken";
+      $c = 10000;
+
+      echo $a, " ik heb alleen ", $c, " manieren gevonden ", $b;
+      echo "<br>-Thomas Edison";
+
+      echo "<h1>Opdracht 2</h1>";
+			$a = "die geen fout";
+			$b = "en";
+			$c = "niet.";
+			
+			echo "Mensen ", $a, $b, " maken, werk", $b," ", $c;
+			echo "<br>-Albert Plesman (KLM)";
+
+			echo "<h1>Opdracht 3</h1>";
+			$a = "ry";
+			$b = ", but";
+			$c = "fail";	
+
+			echo "T", $a, " and ", $c, $b, " never ", $c, " to t", $a, ".";
+			echo "<br>-Tyra Banks";
+
+			echo "<h1>Opdracht 4</h1>";
+			$a = "\"omgaan";
+			$b = "met";
+			$c = "teleu";
+			
+			echo "De cursus ", $a, " ", $b, " ", $c, "rstellingen\" kan vanavond helaas niet doorgaan.";
+			echo "<br>-Herman Finkers";	
+
+			echo "<h1>Opdracht 5</h1>";
+	  
+		 	?>
+  </body>
+</html>
